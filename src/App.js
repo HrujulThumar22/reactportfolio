cb4888c51d803f3130a8b855dcfeb53f074a8292
HrujulThumar22 <hrujul.a.thumar@gmail.com>
2021-05-22 21:11:50 +0530

    About section added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import './App.css';
 import MyNavbar from './components/navbar/navbar'
 import MyCarousel from './components/carousel/carousel'
 import TitleMessage from './components/titleMesssage/titlemessage'
+import About from './pages/about/about'
 
+import Fade from 'react-reveal'
+import Container from "react-bootstrap/Container";
+import { Parallax } from "react-parallax";
 const App=()=> {
   return (
-    <div>
+    <div className="App" style={{ position: "relative" }}>
       <MyNavbar/>
       <MyCarousel/> 
       <TitleMessage/>
+      <div>
+      <Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={require("./assets/img/parallex/background.webp")}
+          bgImageAlt=""
+          strength={-200}
+        >
+          <Container className="container-box rounded">
+              <Fade duration={500}>
+                <About />
+              </Fade>
+            </Container>
+        </Parallax>
+      </div>
       My Portfolio
 
     </div>

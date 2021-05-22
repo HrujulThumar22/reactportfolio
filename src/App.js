0fc1db711d153106f441a685555880fc37b96b8b
HrujulThumar22 <hrujul.a.thumar@gmail.com>
2021-05-22 18:29:45 +0530

    Added carousel scroll-down typewritter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import MyNavbar from './components/navbar/navbar'
+import MyCarousel from './components/carousel/carousel'
+import TitleMessage from './components/titleMesssage/titlemessage'
+
 const App=()=> {
   return (
     <div>
-      <MyNavbar/>My Portfolio
+      <MyNavbar/>
+      <MyCarousel/> 
+      <TitleMessage/>
+      My Portfolio
+
     </div>
   );
 }

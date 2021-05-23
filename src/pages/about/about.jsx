0b95b04d93b7179a195738ad1609b1129a510d53
HrujulThumar22 <hrujul.a.thumar@gmail.com>
2021-05-23 23:39:05 +0530

    about me page updated

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./about.css";
-import Profile from "../../assets/img/profile/profile.webp";
+import Profile from "../../assets/img/profile/profile.jpg";
 import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
 
@@ -14,24 +14,21 @@ const About = () => {
                 <h1 className="pt-3 text-center font-details pb-3">ABOUT ME</h1>
                 <Container>
                     <Row className="pt-3 pb-5 align-items-center">
-                        <Col xs={12} md={6}>
+                        <Col xs={12} md={4}>
                             <Row className="justify-content-center mb-2 mr-2 ">
-                                <Image className="profile justify-content-end" alt="profile" src={Profile} thumbnail fluid />
+                                <Image className="profile justify-content-end" alt="profile" src={Profile} thumbnail fluid rounded />
                             </Row>
                         </Col>
-                        <Col xs={12} md={6}>
-                            <Row className=" align-items-start p-2 my-details rounded">
-                                Hi there! I am <strong>&nbsp;Anand Kumar Jha</strong>
-                                <br />A passionate programmer and a Coach, born and brought up in India. I am a Full Stack Web Developer with React.js, Redux, Express.js, Node.js, and PostgreSQL as my tech stack.
-                                <br />
-                                In 2019, I successfully completed my Engineering with specialization in 'Electronics and Telecommunication'.
-                                <br />
-                                Working with the clients, my goal is always driven towards providing amazing experience with the best level of quality and service to them.
-                                <br />
-                                Along with that, I also help people as a COACH on their journey of becoming a professional programmer. 
-                                <br />I love learning about new technologies, what problems are they solving and How can I use them to build better and scalable products.
-                                <br /> <br />
-                                <Col className="d-flex justify-content-center flex-wrap">
+                        <Col xs={12} md={8}>
+                            Hi there! I am <strong className="titleText">&nbsp;Hrujul Thumar</strong>
+                            <br />A passionate programmer, born and brought up in India. I am a Full Stack Web Developer with React.js, Express.js, Node.js, and MySQL, MongoDB as my tech stack.
+                            <br/>I persuing B.Tech in Computer Science and Engineering from Nirma University.
+                            <hr/>
+                            <strong><span className="titleText">Career Objective:</span></strong>
+                            <br/>To pursue a job opportunity in a competitive environment that will challenge me to push my boundaries and expand my knowledge in the field of computer science while allowing me to add value to the dynamics of the company.
+                            <hr/>
+
+                                <Row>
                                     <div>
                                         <a href="#contact">
                                             <Button className="m-2" variant="outline-primary">
@@ -40,28 +37,27 @@ const About = () => {
                                         </a>
                                     </div>
                                     <div>
-                                        <a href="https://drive.google.com/file/d/1Snbq3x4Itk_ZN4c8J96aNdp4SunXR08Y/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                                        <a href="https://drive.google.com/file/d/1RBKxCX9AE4xeVvQMjM56hlRsBYELri5z/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                                         <Button className="m-2" variant="outline-success">
                                             My Resume
                                         </Button>
                                         </a>
                                     </div>
                                     <div>
-                                        <a href="https://github.com/akjha96" target="_blank" rel="noopener noreferrer">
+                                        <a href="https://github.com/HrujulThumar22" target="_blank" rel="noopener noreferrer">
                                         <Button className="m-2" variant="outline-dark">
                                             GitHub
                                         </Button>
                                         </a>
                                     </div>
                                     <div>
-                                        <a href="https://www.linkedin.com/in/anand-kumar-jha-745798a4/" target="_blank" rel="noopener noreferrer">
+                                        <a href="https://www.linkedin.com/in/hrujul-thumar-b81662183/" target="_blank" rel="noopener noreferrer">
                                         <Button className="m-2" variant="outline-info">
                                             LinkedIn
                                         </Button>
                                         </a>
                                     </div>
-                                </Col>
-                            </Row>
+                                </Row>
                         </Col>
                     </Row>
                 </Container>

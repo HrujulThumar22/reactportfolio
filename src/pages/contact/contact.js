9f1bfa80d7cd9864ee8c1167299f225d3d8897eb
HrujulThumar22 <hrujul.a.thumar@gmail.com>
2021-05-23 23:39:52 +0530

    contact page updated

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -7,8 +7,8 @@ import "./contact.css";
 
 const ContactForm = () => {
     return (
-        <div id="contact">
-        <h1 className="pt-3 text-center font-details-b pb-3">CONTACT ME</h1>
+        <div className="container" id="contact">
+        <h1 className="pt-3 text-center text-dark font-details-b pb-3">CONTACT ME</h1>
         <Jumbotron className="contact-jumbotron">
             <Row>
             <Col className="d-flex justify-content-center flex-wrap">

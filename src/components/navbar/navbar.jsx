9ab86524d40abff4593c4c21e595ffe08c1eb5c1
HrujulThumar22 <hrujul.a.thumar@gmail.com>
2021-05-23 23:39:17 +0530

    navbar updated

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React,{useState} from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import Logo from "../../assets/icons/logow.webp";
+import Logo from "../../assets/icons/logo.png";
 import "./navbar.style.css";
 
 const MyNavbar = () => {
+    const [active, setactive] = useState([true,false,false,false,false,false]);
+
+    const OnSelect=(e)=>{
+        console.log(e);
+        switch(e){
+            case "#home": setactive([true,false,false,false,false,false]);break;
+            case "#about": setactive([false,true,false,false,false,false]);break;
+            case "#skills": setactive([false,false,true,false,false,false]);break;
+            case "#education": setactive([false,false,false,true,false,false]);break;
+            case "#projects": setactive([false,false,false,false,true,false]);break;
+            case "#contact": setactive([false,false,false,false,false,true]);break;
+            default:break;
+        }
+    }
   return (
-    <div>
         <Navbar
             fixed="top"
             variant="dark"
@@ -22,17 +35,16 @@ const MyNavbar = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto ">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#about">About</Nav.Link>
-                        <Nav.Link href="#skills">Skills</Nav.Link>
-                        <Nav.Link href="#experience">Experience</Nav.Link>
-                        <Nav.Link href="#projects">Projects</Nav.Link>
-                        <Nav.Link href="#contact">Contact</Nav.Link>
+                        <Nav.Link class="nav-link" active={active[0]} onSelect={OnSelect} href="#home">Home</Nav.Link>
+                        <Nav.Link class="nav-link" active={active[1]} onSelect={OnSelect} href="#about">About</Nav.Link>
+                        <Nav.Link class="nav-link" active={active[2]} onSelect={OnSelect} href="#skills">Skills</Nav.Link>
+                        <Nav.Link class="nav-link" active={active[3]} onSelect={OnSelect} href="#education">Education</Nav.Link>
+                        <Nav.Link class="nav-link" active={active[4]} onSelect={OnSelect} href="#projects">Projects</Nav.Link>
+                        <Nav.Link class="nav-link" active={active[5]} onSelect={OnSelect} href="#contact">Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </div>
         </Navbar>
-    </div>
   );
 };
 

6a947782114509edaba53fad54f3bd85d2e9b938
HrujulThumar22 <hrujul.a.thumar@gmail.com>
2021-11-04 12:55:36 +0530

    Add updated resume

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -37,7 +37,7 @@ const About = () => {
                                         </a>
                                     </div>
                                     <div>
-                                        <a href="https://drive.google.com/file/d/1RBKxCX9AE4xeVvQMjM56hlRsBYELri5z/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                                        <a href="https://drive.google.com/file/d/1tPmlBUEyd_UMjBnzhSMCkJMo2ncrqrcb/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                                         <Button className="m-2" variant="outline-success">
                                             My Resume
                                         </Button>
